27f139180b23adb8057cbb4bda739a9d3e53878a
Diego Babino <diego@babino.ch>
2026-06-02 20:09:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("MAIN JS LOADED");
+fetch('nav.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('nav-container').innerHTML = data;
+
+        // Lien actif
+        const currentPage = window.location.pathname.split('/').pop();
+
+        document.querySelectorAll('.sidenav__projects a').forEach(link => {
+            if (link.getAttribute('href') === currentPage) {
+                link.classList.add('active');
+            }
+        });
+
+
+        // Menu mobile
+        const menuBtn = document.querySelector('.menu-toggle');
+        const sidenav = document.querySelector('.sidenav');
+
+        if (menuBtn && sidenav) {
+            menuBtn.addEventListener('click', () => {
+                sidenav.classList.toggle('open');
+            });
+        }
+    });
